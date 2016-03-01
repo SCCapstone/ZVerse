c92db394f59c7d3e60734c759b67cb0b8db9f1d6
clymerc <clymerc@email.sc.edu>
2016-02-29 19:23:10 -0500

    Admin can upload javascript file on admin-only page

--- ZVerseBrickProject/ZVerseBrickProject/Admin/AdminPage.aspx
+++ ZVerseBrickProject/ZVerseBrickProject/Admin/AdminPage.aspx
@@ -1,6 +1,16 @@
 ﻿<%@ Page Title="AdminPage" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AdminPage.aspx.cs" Inherits="ZVerseBrickProject.Admin.AdminPage" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <p>
-        you have accessed the admin only page!
-    </p>
+    
+    <h1>Welcome, Admin!</h1>
+    <hr />
+    <h3>Manage your models here</h3>
+
+    <div id="uploadForm">
+        <p>Upload your javascript file here: </p>
+        <asp:FileUpload id="FileUploadControl" runat="server" />
+        <asp:Button runat="server" id="UploadButton" text="Upload" onclick="UploadButton_Click" />
+        <br /><br />
+        <asp:Label runat="server" id="StatusLabel" text="" />
+    </div>
+
 </asp:Content>
--- ZVerseBrickProject/ZVerseBrickProject/Admin/AdminPage.aspx.cs
+++ ZVerseBrickProject/ZVerseBrickProject/Admin/AdminPage.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,28 @@ namespace ZVerseBrickProject.Admin
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                try
+                {
+                    if (FileUploadControl.PostedFile.ContentType == "application/javascript")
+                    {
+                        string filename = Path.GetFileName(FileUploadControl.FileName);
+                        FileUploadControl.SaveAs(Server.MapPath("~/Uploaded_Files/") + filename);
+                        StatusLabel.Text = "Upload status: File uploaded!";
+                    }
+                    else
+                        StatusLabel.Text = "Upload status: Only Javascript files are accepted!";
+                }
+                catch (Exception ex)
+                {
+                    StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
         }
     }
 }
