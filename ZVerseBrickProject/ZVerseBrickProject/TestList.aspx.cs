d1957493b1f8efb3d5ac972e65898c16b59de80e
mingwongithub <email4ming@yahoo.com>
2016-03-03 11:51:10 -0500

    Generate a listing of brick model

---
+++ ZVerseBrickProject/ZVerseBrickProject/TestList.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ZVerseBrickProject.Models;
+using System.Web.ModelBinding;
+
+
+namespace ZVerseBrickProject
+{
+    public partial class TestForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IQueryable<Brick> GetBricks()
+        {
+            var _db = new ZVerseBrickProject.Models.ProductContext();
+            IQueryable<Brick> query = _db.Bricks;
+            return query;
+        }
+
+
+    }
+}
