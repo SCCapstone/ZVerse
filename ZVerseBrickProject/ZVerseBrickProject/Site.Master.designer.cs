c1449a936cda17dc227d458064911a8f7501a1c7
mingwongithub <email4ming@yahoo.com>
2016-03-20 21:25:50 -0400

    Making navbar look more tab-like

--- ZVerseBrickProject/ZVerseBrickProject/Site.Master.designer.cs
+++ ZVerseBrickProject/ZVerseBrickProject/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace ZVerseBrickProject {
     public partial class SiteMaster {
         
         /// <summary>
+        /// Stylesheets control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Stylesheets;
+        
+        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
