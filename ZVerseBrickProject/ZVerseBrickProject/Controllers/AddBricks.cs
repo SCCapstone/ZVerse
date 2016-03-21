04f8bfa105f1b52d6ad327105844555c0f8897f8
mingwongithub <email4ming@yahoo.com>
2016-03-20 20:25:34 -0400

    Create the function to add new brick models and product into the database

---
+++ ZVerseBrickProject/ZVerseBrickProject/Controllers/AddBricks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ZVerseBrickProject.Models;
+
+namespace ZVerseBrickProject.Controllers
+{
+    public class AddBricks
+    {
+        public int AddBrick(int BrickID, string BrickName, string BrickDesc, string BrickPrice, string BrickImagePath, string BrickIncript)
+        {
+            var myBrick = new Brick();
+            myBrick.BrickID = BrickID; 
+            myBrick.BrickName = BrickName;
+            myBrick.Description = BrickDesc;
+            myBrick.UnitPrice = Convert.ToDouble(BrickPrice);
+            myBrick.ImagePath = BrickImagePath;
+            myBrick.Incription = BrickIncript;
+
+
+            using (ProductContext _db = new ProductContext())
+            {
+                _db.Bricks.Add(myBrick);
+                _db.SaveChanges();
+            }
+
+            int id = myBrick.BrickID; 
+            return id;
+        }
+
+    }
+}
---
+++ ZVerseBrickProject/ZVerseBrickProject/Controllers/AddProducts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ZVerseBrickProject.Models;
+
+namespace ZVerseBrickProject.Controllers
+{
+    public class AddProducts
+    {
+        public int AddProduct(string ProductName, string ProductDesc, string ProductPrice, string ProductCategory, string ProductImagePath, string ProductIncript)
+        {
+            var myProduct = new Product();
+            myProduct.ProductName = ProductName;
+            myProduct.Description = ProductDesc;
+            myProduct.UnitPrice = Convert.ToDouble(ProductPrice);
+            myProduct.ImagePath = ProductImagePath;
+            myProduct.CategoryID = Convert.ToInt32(ProductCategory);
+            myProduct.Incription = ProductIncript;
+
+
+            using (ProductContext _db = new ProductContext())
+            {
+                _db.Products.Add(myProduct);
+                _db.SaveChanges();
+            }
+
+            int id = myProduct.ProductID; 
+            return id;
+        }
+
+    }
+}
