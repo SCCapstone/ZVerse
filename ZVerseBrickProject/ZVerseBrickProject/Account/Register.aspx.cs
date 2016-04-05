136a2069e8009cba5d16a4889e0788854af8de88
alexabreeland <breelana@email.sc.edu>
2016-04-05 16:36:34 -0400

    setting up for forgot password

--- ZVerseBrickProject/ZVerseBrickProject/Account/Register.aspx.cs
+++ ZVerseBrickProject/ZVerseBrickProject/Account/Register.aspx.cs
@@ -20,6 +20,7 @@ namespace ZVerseBrickProject.Account
             if (result.Succeeded)
             {
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+
                 string code = manager.GenerateEmailConfirmationToken(user.Id);
                 string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
                 manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
